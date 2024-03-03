e4cb410ef3141c8d53b3072b4961705b2c7aea2f
Taha Qamar <tahakamar97@gmail.com>
2024-03-04 02:49:39 +0500

    add two file

---
+++ app.js
@@ -0,0 +1,90 @@
+// var pets = ["cat","dog","fish","camel","monkey"];
+// console.log(pets);
+// pets.push("rats","bear");
+// console.log(pets);
+// pets.pop("rats");
+// console.log(pets);
+// pets.shift();
+// console.log(pets);
+// pets.unshift("goat","horse");
+// console.log(pets);
+// // typeof(pets);
+// // typeof(pets[2]);
+// pets.splice(2,3,"sheep");
+// console.log(pets);
+// newPet = pets.slice(1,4)
+// console.log(newPet);
+// newPet = pets.slice(3);
+// console.log(newPet);
+
+// LOOP
+
+// for (var i =10; i >= 0 ; i--){
+//   document.write(i + "<br>");
+// }
+
+// for (var i =20; i >=0 ; i=i-4){
+//   document.write(i + "<br>");
+//  }
+ 
+// Q1
+// var name = ["Taha"];
+// var lastname = ["Qamar"];
+// var name1 = [name + lastname ];
+// var age = [23];
+// var value = [true,false];
+// var mixed = [name1 + age + value];
+
+// document.write(mixed)
+
+
+// var qualification = [ "<h1>" + "QUALIFICATION :" + "</h1>"+ "<h2>" +"<br>" + "1) SSC" +"<br>" + "2) HSC"+"<br>" +"3) BCS"+"<br>"+ "4) BS"+"<br>"+ "5) BCOM"+"<br>"+"6) MS"+"<br>"+ "7) M. Phil." + "<br>" + "8) PhD"+"</h2>"]
+// document.write(qualification)
+
+// Q8
+ 
+// var eng = 60;
+// var math = 60;
+// var isl = 70;
+// var urdu = 80;
+// var total = eng + math + isl + urdu;
+// var percentage = total/400*100;
+// if(percentage >= 80 && percentage <100){
+//   document.write("A+")
+// }
+// else if(percentage >= 70 && percentage <80){
+//   document.write("A")
+// }
+// else if(percentage >= 60 && percentage <70){
+//   document.write("B")
+// }
+// else if(percentage >= 50 && percentage <60){
+//   document.write("C")
+// }
+// else if(percentage >= 40 && percentage <50){
+//   document.write("D")
+// }
+// else if(percentage >= 33 && percentage <40){
+//   document.write("E")
+// }
+// if(percentage >= 0 && percentage <33){
+//   document.write("FAIL")
+// }
+
+// Q9
+
+// var scores =" students of scores ";
+// var num = [": 320","230","430","120"];
+// console.log( scores +  pets  );
+
+// Q10
+
+// var scores =" Ordered students of scores ";
+// var pets = [": 120","430","230","320"];
+// console.log( scores +  pets  );
+
+// Q11
+
+
+
+
